0cb84ec3c39c963d27955e741dd6bc43645d4a0e
Dragon Master <tigerich@users.noreply.github.com>
2020-12-31 09:25:15 +0800

    Update CreateAccountDemo.java
    refine CreateAccountDemo

--- demo/src/main/java/CreateAccountDemo.java
+++ demo/src/main/java/CreateAccountDemo.java
@@ -11,14 +11,14 @@ public class CreateAccountDemo {
 
         EosRpcService eosRpcService = new EosRpcService("http://sh-test.vm.mgps.me:8888");
 
-        String pk = "5Jg9EGiVX7b8HSNyPGHVDTxKDwe4VusZhu4GDNZEmxJsovDKiFe";
+        String ownerPubKey = "EOS6xt9rXurXqp7TTx4FkPff9A28BoZXbAS6aU2Rk1cEjJNx6icqr";
+        String activePubKey = ownerPubKey; //can choose a different one, using the same one is for simplicity.
+        String privKey = "5Jg9EGiVX7b8HSNyPGHVDTxKDwe4VusZhu4GDNZEmxJsovDKiFe";
         String creator = "mgpheying111";
         String newAccount = "mgp.testdemo";
-        String publicKey = "EOS6xt9rXurXqp7TTx4FkPff9A28BoZXbAS6aU2Rk1cEjJNx6icqr";
-
+        
         try {
-
-            Transaction account = eosRpcService.createAccount(pk, creator, newAccount, publicKey, publicKey, 4300L, "0.0004 MGP", "0.0004 MGP", 0L);
+            Transaction account = eosRpcService.createAccount(privKey, creator, newAccount, ownerPubKey, activePubKey, 4300L, "0.0004 MGP", "0.0004 MGP", 0L);
 
         } catch (ApiException e) {
             System.err.println(e.getError().getError().getWhat());
